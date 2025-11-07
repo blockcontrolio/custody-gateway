16205c6245569ca81ffbacee6f47d2fa0b7596bf
web3yurii <web3yurii@gmail.com>
2025-11-07 19:05:30 +0200

    Add channel module and erc7824 dependency

---
+++ src/channel/channel.controller.ts
@@ -0,0 +1,8 @@
+import {Controller} from "@nestjs/common";
+import {ChannelService} from "./channel.service";
+
+@Controller({})
+export class ChannelController {
+    constructor(private readonly channelService: ChannelService) {
+    }
+}
